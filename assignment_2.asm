dc3d7bf5c5041535338bc14ee8ae4091ff6f9dd3
YashwantBhosale <yashwantbhosale07@gmail.com>
2024-12-02 00:04:50 +0530

    add: program 2: encoded scan 2 key lockout

---
+++ assignment_2.asm
@@ -0,0 +1,25 @@
+; write a program in 8086 assembly language to demonstrate Decoded Scan key matrix mode of 8279
+
+	MOV AL, 01H ; encoded scan 2 key lockout
+	OUT 31H, AL
+
+	MOV AL, 3FH ; program clock
+	OUT 31H, AL
+	
+	MOV AL, 50H ; command word for reading FIFO
+	OUT 31H, AL
+
+READ:
+	IN AL, 31H ; read FIFO status
+
+	AND AL, 07H ; check last 3 bits (they indicate number of keys pressed)
+	
+	JZ READ ; keep checking untill a keypress occurs
+
+	; following code will execute when keypress will occur
+	IN AL, 30H ; read data from FIFO ram
+	
+	MOV BX, 02H
+	CALL FAR F000:0D5EH ; in built function to display contents of AL
+
+	JMP READ ; jump back to READ routine
